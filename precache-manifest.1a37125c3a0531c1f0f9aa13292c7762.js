9e696200e854a64dbeeea137fb094bc19b7c576d
Leonid660 <leonid_zolotovky@bk.ru>
2021-09-04 11:19:30 +0300

    Updates

---
+++ precache-manifest.1a37125c3a0531c1f0f9aa13292c7762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1e59c39542ecc747603d5079246717",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "ab642400ba60428224eb",
+    "url": "/homeworks/static/css/main.8e4132ed.chunk.css"
+  },
+  {
+    "revision": "179258f172d7137d9121",
+    "url": "/homeworks/static/js/2.b0dc440a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworks/static/js/2.b0dc440a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab642400ba60428224eb",
+    "url": "/homeworks/static/js/main.90480290.chunk.js"
+  },
+  {
+    "revision": "9263b9c20382da27b976",
+    "url": "/homeworks/static/js/runtime-main.1f7d4fd3.js"
+  }
+]);
